867afd3547c542e38b5749f5db1d34ca8f375bca
kellworks <kchidothi@outlook.com>
2024-03-30 22:47:21 +0200

    commit for pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <div>
 <h1 class="text-indigo-600 text-5xl font-bold">OpenDataMalawi</h1>
+<div class="bg-indigo-600 text-white px-4 py-1.5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus maiores ipsum incidunt qui iure doloribus non ab officia dolore odit odio consequatur necessitatibus ullam ad soluta tenetur, voluptate nobis laborum?</div>
 </div>
 
 <script lang="ts">
